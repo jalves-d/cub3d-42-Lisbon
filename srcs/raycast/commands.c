eaf600d739c08ab13fb3a46b07735abb96951401
Flavio Almeida <falmeida@c1r20s9.42lisboa.com>
2021-10-29 16:03:38 +0100

    m

--- srcs/raycast/commands.c
+++ srcs/raycast/commands.c
@@ -3,44 +3,46 @@
 int	key_print(int key, t_game *game)
 {
 	t_view	*view;
+	t_map	*map;
+
+	double oldDirX;
+	double oldPlaneX;
+	double oldDirX;
+	double oldPlaneX;
 
 	view = game->view;
+	map = game->map;
 	if (key == ESC)
 		close_win(game);
 	if (key == W)
 	{
-		if (game->map->rmap[(int)(view->posX + view->dirX * view->moveSpeed)][(int)(view->posY)] == '0')
+		if (map->rmap[(int)(view->posX + view->dirX * view->moveSpeed)][(int)(view->posY)] == '0')
 			view->posX += view->dirX * view->moveSpeed;
-		if (game->map->rmap[(int)(view->posX)][(int)(view->posY + view->dirY * view->moveSpeed)] == '0')
+		if (map->rmap[(int)(view->posX)][(int)(view->posY + view->dirY * view->moveSpeed)] == '0')
 			view->posY += view->dirY * view->moveSpeed;
 	}
-	//move backwards if no wall behind you
 	if (key == S)
 	{
-		if (game->map->rmap[(int)(view->posX - view->dirX * view->moveSpeed)][(int)(view->posY)] == '0')
+		if (map->rmap[(int)(view->posX - view->dirX * view->moveSpeed)][(int)(view->posY)] == '0')
 			view->posX -= view->dirX * view->moveSpeed;
-		if (game->map->rmap[(int)(view->posX)][(int)(view->posY - view->dirY * view->moveSpeed)] == '0')
+		if (map->rmap[(int)(view->posX)][(int)(view->posY - view->dirY * view->moveSpeed)] == '0')
 			view->posY -= view->dirY * view->moveSpeed;
 	}
-	//rotate to the right
 	if (key == D)
 	{
-		//both camera direction and camera plane must be rotated
-		double oldDirX = view->dirX;
+		view->dirX;
 		view->dirX = view->dirX * cos(-view->rotSpeed) - view->dirY * sin(-view->rotSpeed);
 		view->dirY = oldDirX * sin(-view->rotSpeed) + view->dirY * cos(-view->rotSpeed);
-		double oldPlaneX = view->planeX;
+		view->planeX;
 		view->planeX = view->planeX * cos(-view->rotSpeed) - view->planeY * sin(-view->rotSpeed);
 		view->planeY = oldPlaneX * sin(-view->rotSpeed) + view->planeY * cos(-view->rotSpeed);
 	}
-	//rotate to the left
 	if (key == A)
 	{
-		//both camera direction and camera plane must be rotated
-		double oldDirX = view->dirX;
+		view->dirX;
 		view->dirX = view->dirX * cos(view->rotSpeed) - view->dirY * sin(view->rotSpeed);
 		view->dirY = oldDirX * sin(view->rotSpeed) + view->dirY * cos(view->rotSpeed);
-		double oldPlaneX = view->planeX;
+		view->planeX;
 		view->planeX = view->planeX * cos(view->rotSpeed) - view->planeY * sin(view->rotSpeed);
 		view->planeY = oldPlaneX * sin(view->rotSpeed) + view->planeY * cos(view->rotSpeed);
 	}
